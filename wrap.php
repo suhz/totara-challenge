47e59c36320d9adb8928e6a3a4d38d5bd63037ac
Suhaimi Amir <suhaimi.hz@gmail.com>
2017-09-05 20:11:13 +1200

    initial commit

---
+++ wrap.php
@@ -0,0 +1,40 @@
+<?php
+
+function wrap($string, $length) {
+
+    // we need to initialize some variables (and array)
+    $lines = 1;
+    $wraped = '';
+    $stringArr[$lines] = [];
+
+    // break the string into words
+    $words = explode(' ', $string);
+
+    // here we go, let the journey begins
+    foreach ($words as $word) {
+
+        // what we currently have in our line
+        $currentLine = implode(' ',$stringArr[$lines]);
+
+        // will the new line length go over $length? Calculating...
+        $newLineLength = strlen($currentLine) + strlen($word);
+
+        // looks like the new line will go over the limit
+        if ($newLineLength >= $length) {
+            $lines++; // go to the next line please
+        }
+
+        // add the word into current line that we are working for
+        $stringArr[$lines][] = $word;
+    }
+
+    // gather everything into a string, and add the new line character
+    foreach ($stringArr as $lines) {
+        $wraped .= implode(' ', $lines) . "\n";
+    }
+
+    // here we go, our wrapped string :)
+    return $wraped;
+}
+
+echo wrap('testing one two three four five', 10);
